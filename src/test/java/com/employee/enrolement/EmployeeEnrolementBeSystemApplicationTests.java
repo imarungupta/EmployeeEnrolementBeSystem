75ff92f53af0d803ddb997dc51a59bb7dae3fa48
imarungupta <imarungupta@gmail.com>
2022-07-17 11:07:35 +0530

    Project Structure

---
+++ src/test/java/com/employee/enrolement/EmployeeEnrolementBeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.enrolement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeEnrolementBeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
